e86e15b853072a527253df2a317874949edd44d6
Ruffiana <ruffiana.plays@gmail.com>
2024-12-17 14:04:38 -0600

    added string normalize function

--- modhelper/main.py
+++ modhelper/main.py
@@ -32,12 +32,15 @@ class ModHelper(commands.Cog):
         self.logger.info(f"{self.__class__.__name__} v({__version__}) initialized!")
         self.logger.info("-" * 32)
 
+    def normalize_string(self, s: str) -> str:
+        return s.strip().lower()
+
     @commands.command()
     async def find(
         self,
         ctx: commands.Context,
-        name: str,
-        score: int = 70,
+        username: str,
+        score: int = 85,
         results: int = 5,
     ) -> None:
         """
@@ -45,29 +48,34 @@ class ModHelper(commands.Cog):
 
         Args:
             ctx (commands.Context): The command context.
-            name (str): The user or display name to search for.
-            score (int, optional): The minimum score threshold for matching. Defaults to 70.
+            username (str): The username to search for.
+            score (int, optional): The minimum score threshold for matching. Defaults to 85.
             results (int, optional): The maximum number of results to display. Defaults to 5.
         """
         members: List[discord.Member] = ctx.guild.members
         member_names: List[Tuple[str, str]] = [
-            (member.display_name, member.name) for member in members
+            (
+                self.normalize_string(member.display_name),
+                self.normalize_string(member.name),
+            )
+            for member in members
         ]
         search_targets: List[str] = [
             f"{display_name} ({name})" for display_name, name in member_names
         ]
+        normalized_username = self.normalize_string(username)
 
         found_users: List[Tuple[str, int]] = process.extract(
-            name, search_targets, limit=results
+            normalized_username, search_targets, limit=results
         )
-        await self.show_results(ctx, name, found_users, score, members)
+        await self.show_results(ctx, username, found_users, score, members)
 
     async def show_results(
         self,
         ctx: commands.Context,
         username: str,
         found_users: List[Tuple[str, int]],
-        min_score: int,
+        score: int,
         members: List[discord.Member],
     ) -> None:
         """
@@ -77,22 +85,24 @@ class ModHelper(commands.Cog):
             ctx (commands.Context): The command context.
             username (str): The username searched for.
             found_users (List[Tuple[str, int]]): List of found users with scores.
-            min_score (int): The minimum score threshold for matching.
+            score (int): The minimum score threshold for matching.
             members (List[discord.Member]): List of guild members.
         """
-        for user, score in found_users:
+        for user, match_score in found_users:
             # Only include matches that meet the minimum score
-            if score < min_score:
+            if match_score < score:
                 continue
 
-            display_name, name = user.split(" (")
+            _, name = user.split(" (")
             name = name.rstrip(")")
             member = discord.utils.get(members, name=name)
-            if member:
-                await ctx.send(f"### {member.display_name} ({member.name}) - {score}%")
-                await ctx.send(f"{member.id}")
+
+            if not member:
+                continue
+
+            # send userID on separate line so it's easier to copy on mobile devices
+            await ctx.send(f"### {member.display_name} ({member.name})")
+            await ctx.send(f"{member.id}")
 
         if not found_users:
-            await ctx.send(
-                f"No matches found for '{username}' with the minimum score of {min_score}."
-            )
+            await ctx.send(f"No good matches found for '{username}'.")

1d244a679d1b13ba981dd4fadf796caa97866c00
Ruffiana <ruffiana.plays@gmail.com>
2024-12-18 17:02:24 -0600

    removed uses of pluralize on labels

--- roleplay/settings.py
+++ roleplay/settings.py
@@ -28,7 +28,7 @@ from redbot.core.bot import Red
 from redbot.core.utils.chat_formatting import humanize_list
 
 from . import const
-from .strings import get_indefinite_article, pluralize
+from .strings import get_indefinite_article
 from .user_settings import USER_SETTINGS
 from .users import Manager
 
@@ -165,7 +165,7 @@ class Settings:
                 users = await self.users_manager.list_users(
                     ctx.author, property, as_display=True
                 )
-                label = pluralize(values.get("label", property.capitalize()))
+                label = values.get("label", property.capitalize())
                 if not users or users == "None":
                     await ctx.send(f'No "{label}" for {ctx.author.display_name}.')
                 else:
@@ -309,7 +309,6 @@ class Settings:
             default = values.get("default")
             emoji = values.get("emoji")
             label = values.get("label")
-            label = pluralize(label) if isinstance(default, list) else label
             description = values.get("description")
             line = f"* {emoji} **{label}** - {description}"
             desc_lines.append(line)
@@ -333,7 +332,7 @@ class Settings:
                 users = await self.users_manager.list_users(
                     member, property, as_display=True, as_string=True
                 )
-                embed.add_field(name=pluralize(label), value=users, inline=True)
+                embed.add_field(name=label, value=users, inline=True)
             elif data_type is bool:
                 bool_value = await self.config.user(member).get_attr(property)()
                 value = const.TRUE_EMOJI if bool_value else const.FALSE_EMOJI

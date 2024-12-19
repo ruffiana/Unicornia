36960c7a2bc59f353c7f3c8489e13dc00d220819
Ruffiana <ruffiana.plays@gmail.com>
2024-12-19 12:11:32 -0600

    version bump

--- roleplay/__init__.py
+++ roleplay/__init__.py
@@ -4,13 +4,13 @@ TODO:
     Add "ask" command
 """
 
-__version__ = "2.4.0"
+__version__ = "2.5.0"
 __author__ = "ruffiana"
 __credits__ = ["the.kirin", "neviyn", "fitz.lol"]
 
-from .main import RoleplayCog
+from .main import Roleplay
 
 
 # Your bot's code continues here...
 async def setup(bot):
-    await bot.add_cog(RoleplayCog(bot))
+    await bot.add_cog(Roleplay(bot))

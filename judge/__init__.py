32b946259b9bd148ae0dabacdc1a5c12e51a73c2
Ruffiana <ruffiana.plays@gmail.com>
2024-12-20 02:41:08 -0600

    version bump

--- judge/__init__.py
+++ judge/__init__.py
@@ -1,11 +1,11 @@
 """Template for redbot cog"""
 
-__version__ = "0.2.0-alpha"
+__version__ = "0.3.0-alpha"
 __author__ = "Ruffiana"
 
-from .main import JudgesCog
+from .main import JudgeCog
 
 
 # Your bot's code continues here...
 async def setup(bot):
-    await bot.add_cog(JudgesCog(bot))
+    await bot.add_cog(JudgeCog(bot))

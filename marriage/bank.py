f0a722be7aa65c4f8ea683cba71284de54a878a1
Ruffiana <ruffiana.plays@gmail.com>
2024-12-25 11:52:59 -0600

    Add custom bank module with currency handling methods

---
+++ marriage/bank.py
@@ -0,0 +1,26 @@
+class bank:
+    """Overwrites 'bank' module"""
+
+    @staticmethod
+    async def get_currency_name(*args, **kwargs):
+        return "NOT Slut Points"
+
+    @staticmethod
+    async def get_balance(*args, **kwargs):
+        return 0
+
+    @staticmethod
+    async def can_spend(*args, **kwargs):
+        return True
+
+    @staticmethod
+    async def withdraw_credits(*args, **kwargs):
+        return True
+
+    @staticmethod
+    async def withdraw_credits(*args, **kwargs):
+        return True
+
+    @staticmethod
+    async def deposit_credits(*args, **kwargs):
+        return True

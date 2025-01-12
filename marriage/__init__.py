78930321e004bc383d8794ed23b0f0bb8026868c
Ruffiana <ruffiana.plays@gmail.com>
2025-01-11 20:40:13 -0600

    Bump version to 2.0.0 and simplify setup process by removing unnecessary task creation

--- marriage/__init__.py
+++ marriage/__init__.py
@@ -7,26 +7,10 @@ https://github.com/elijabesu/SauriCogs/tree/master/marriage
 Main focus has been on decoupling everything from the currency systems
 """
 
-__version__ = "1.7.0"
-
-
-from asyncio import create_task
+__version__ = "2.0.0"
 
 from .main import Marriage
 
 
-async def setup_after_ready(bot):
-    await bot.wait_until_red_ready()
-    cog = Marriage(bot)
-    for name, command in cog.all_commands.items():
-        if not command.parent:
-            if bot.get_command(name):
-                command.name = f"m{command.name}"
-            for alias in command.aliases:
-                if bot.get_command(alias):
-                    command.aliases[command.aliases.index(alias)] = f"m{alias}"
-    await bot.add_cog(cog)
-
-
 async def setup(bot):
-    create_task(setup_after_ready(bot))
+    await bot.add_cog(Marriage(bot))

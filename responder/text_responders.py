6287ff8d7293da1b0fdac4d17c12b67306686319
Ruffiana <ruffiana.plays@gmail.com>
2025-01-15 19:31:19 -0600

    Add typing delay and embed message functionality to TextResponder class

--- responder/text_responders.py
+++ responder/text_responders.py
@@ -1,9 +1,15 @@
 from abc import ABC, abstractmethod
 
 import discord
+import asyncio
+
+from . import const
 
 
 class TextResponder(ABC):
+    parent = None
+    bot = None
+    enabled = False
     ignore_case = True
     delete_after = None
     pattern = ""
@@ -12,6 +18,30 @@ class TextResponder(ABC):
     async def response(self, message: discord.Message, matched_text: str):
         """Define the response to the matched message"""
         pass
+
+    async def delay(self, message: discord.Message, text: str):
+        typing_delay = min(3, len(text) * 0.05)
+        self.parent.logger.info(f"Typing delay: {typing_delay}")
+        async with message.channel.typing():
+            await asyncio.sleep(typing_delay)
+
+    async def embed(self, message: discord.Message, title: str, description: str):
+        await self.delay(message, description)
+        embed = discord.Embed(
+            title=title,
+            description=description,
+            color=const.UNICORNIA_BOT_COLOR,
+        )
+        embed.set_thumbnail(url=self.bot.user.avatar.url)
+        await message.channel.send(embed=embed)
+
+    async def message(self, message: discord.Message, text: str):
+        await self.delay(message, text)
+        return await message.channel.send(text)
+
+    async def reply(self, message: discord.Message, text: str):
+        await self.delay(message, text)
+        return await message.reply(text)
 
     def __str__(self):
         return f"{self.__class__.__name__}(pattern={self.pattern})"

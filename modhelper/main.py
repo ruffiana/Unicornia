38acfc3a63f9e8b17307afcc56a05b70502a6b18
Ruffiana <ruffiana.plays@gmail.com>
2025-01-15 22:09:10 -0600

    Refactor user search functionality to utilize rapidfuzz for improved matching and add Member data class for better member management

--- modhelper/main.py
+++ modhelper/main.py
@@ -12,13 +12,23 @@ Commands:
 import logging
 import re
 from typing import List, Tuple
+from operator import itemgetter
 
 import discord
-from rapidfuzz import process
+from rapidfuzz import process, fuzz
 from redbot.core import commands
 from redbot.core.bot import Red
 
-from . import __author__, __version__, const
+from . import __author__, __version__
+from dataclasses import dataclass
+
+
+@dataclass
+class Member:
+    name: str
+    display_name: str
+    id: int
+    score: int = 0
 
 
 class ModHelperCog(commands.Cog):
@@ -28,14 +38,19 @@ class ModHelperCog(commands.Cog):
         self.logger: logging.Logger = logging.getLogger(
             f"{__name__}.{self.__class__.__name__}"
         )
-        self.logger.setLevel(const.LOGGER_LEVEL)
+        self.logger.setLevel(logging.INFO)
 
         self.logger.info("-" * 32)
         self.logger.info(f"{self.__class__.__name__} v({__version__}) initialized!")
         self.logger.info("-" * 32)
 
-    def normalize_string(self, s: str) -> str:
-        return s.strip().lower()
+    def get_members(self, ctx: commands.Context) -> List[Member]:
+        members = []
+        for member in ctx.guild.members:
+            members.append(
+                Member(id=member.id, name=member.name, display_name=member.display_name)
+            )
+        return members
 
     @commands.command()
     async def find(
@@ -43,7 +58,7 @@ class ModHelperCog(commands.Cog):
         ctx: commands.Context,
         *username: str,
         score: int = 85,
-        results: int = 5,
+        limit: int = 5,
     ) -> None:
         """
         Find a user by username using fuzzy matching.
@@ -52,38 +67,65 @@ class ModHelperCog(commands.Cog):
             ctx (commands.Context): The command context.
             username (str): The username to search for.
             score (int, optional): The minimum score threshold for matching. Defaults to 85.
-            results (int, optional): The maximum number of results to display. Defaults to 5.
+            limit (int, optional): The maximum number of results to display. Defaults to 5.
+
+        Returns:
+            None
         """
         username = " ".join(username)
-        members: List[discord.Member] = ctx.guild.members
-        member_names: List[Tuple[str, str]] = [
-            (
-                self.normalize_string(member.display_name),
-                self.normalize_string(member.name),
-            )
-            for member in members
-        ]
-        search_targets: List[str] = [
-            f"{display_name} ({name})" for display_name, name in member_names
-        ]
-        normalized_username = self.normalize_string(username)
+        matched_members = self.search_members(ctx, username, score_threshold=score)
+        await self.show_results(ctx, username, matched_members[:limit])
 
-        found_users: List[Tuple[str, int]] = process.extract(
-            normalized_username,
-            search_targets,
-            limit=results,
-            scorer=process.fuzz.ratio,
+    def search_members(self, ctx, search_term, score_threshold=85):
+        members = self.get_members(ctx)
+        # Create a list of tuples (id, name) for both name and display_name
+        names = [member.name for member in members]
+        display_names = [member.display_name for member in members]
+
+        # Use rapidfuzz to get the best matches
+        results_names = process.extract(
+            search_term,
+            names,
+            scorer=fuzz.ratio,
+            score_cutoff=score_threshold,
+            limit=len(names),
+        )
+        results_display_names = process.extract(
+            search_term,
+            display_names,
+            scorer=fuzz.ratio,
+            score_cutoff=score_threshold,
+            limit=len(display_names),
         )
 
-        await self.show_results(ctx, username, found_users, score, members)
+        # Combine results from names and display_names
+        # Sort combined results by score in descending order
+        combined_results = results_names + results_display_names
+        combined_results.sort(key=itemgetter(1), reverse=True)
+
+        # Create a set to track added member ids to avoid duplicates
+        added_member_ids = set()
+
+        # Create a list to store the ordered Member objects
+        matched_members = []
+
+        for result in combined_results:
+            member_name, score, index = result
+            for member in members:
+                if (
+                    member.name == member_name or member.display_name == member_name
+                ) and member.id not in added_member_ids:
+                    member.score = score
+                    matched_members.append(member)
+                    added_member_ids.add(member.id)
+
+        return matched_members
 
     async def show_results(
         self,
         ctx: commands.Context,
         username: str,
-        found_users: List[Tuple[str, int]],
-        score: int,
-        members: List[discord.Member],
+        matched_members: List[dict],
     ) -> None:
         """
         Display the search results.
@@ -91,27 +133,15 @@ class ModHelperCog(commands.Cog):
         Args:
             ctx (commands.Context): The command context.
             username (str): The username searched for.
-            found_users (List[Tuple[str, int]]): List of found users with scores.
+            found_users (List[Tuple[str, int, int]]): List of found users with scores.
             score (int): The minimum score threshold for matching.
             members (List[discord.Member]): List of guild members.
         """
-        good_matches = 0
-        for user, match_score in found_users:
-            # Only include matches that meet the minimum score
-            if match_score < score:
-                continue
-
-            match = re.match(r"^(.*) \((.*)\)$", user)
-            if match:
-                name = match.group(2)
-            member = discord.utils.get(members, name=name)
-
-            if not member:
-                continue
-
+        for member in matched_members:
             # send userID on separate line so it's easier to copy on mobile devices
-            await ctx.send(f"### {member.display_name} ({member.name})")
+            await ctx.send(
+                f"### {member.display_name} ({member.name}) - {member.score}%"
+            )
             await ctx.send(f"{member.id}")
-            good_matches += 1
 
-        return await ctx.send(f"Found {good_matches} matches for '{username}'.")
+        return await ctx.send(f"Found {len(matched_members)} matches for '{username}'.")

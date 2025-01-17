38abc3554f4b5d549e9fa841c00c02e0507d00d0
Ruffiana <ruffiana.plays@gmail.com>
2025-01-16 20:38:24 -0600

    Refactor Daddy responder to inherit from TextResponderBase and update response method for improved message handling

--- responder/responders/daddy.py
+++ responder/responders/daddy.py
@@ -4,14 +4,14 @@ import re
 import discord
 from redbot.core.bot import Red
 
-from ..text_responders import TextResponder
+from .text_responder_base import TextResponderBase
 
 
-class Daddy(TextResponder):
+class Daddy(TextResponderBase):
     enabled = True
-    ignore_case = True
     # Match "i'm | i am" at the beginning of the message
     pattern = r"\A(?:i'?\s?a?m\s+)"
+    ignore_case = True
 
     # List of user IDs that will always get a response
     always_respond = [474075064069783552]
@@ -19,19 +19,21 @@ class Daddy(TextResponder):
     never_respond = []
 
     def __init__(self, parent, bot: Red):
+        super().__init__()
         self.parent = parent
         self.bot = bot
 
-    async def response(self, message: discord.Message):
+    async def respond(self, message: discord.Message, target: discord.Member = None):
         if message.author.id in self.never_respond:
             return
 
-        flags = re.IGNORECASE if self.ignore_case else 0
-        match = re.match(self.pattern, message.content, flags)
+        match = re.match(self.pattern, message.content, self.regex_flags)
         name = message.content[match.end() :].strip()
 
         chance = 50 / max(1, len(name))
         self.parent.logger.debug(f"{name} = {chance}% chance to respond")
 
         if message.author.id in self.always_respond or random.randint(0, 99) < chance:
-            await self.reply(message, f"Hi, {name}! I'm your daddy...")
+            await self.send_message(
+                message, f"Hi, {name}! I'm your daddy...", as_reply=True, delay=True
+            )

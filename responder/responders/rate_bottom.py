f4f839830b70d279a1132ee6534b9029a2276f6c
Ruffiana <ruffiana.plays@gmail.com>
2025-01-17 14:17:18 -0600

    Add BottomRate responder for dynamic bottom rating responses

---
+++ responder/responders/rate_bottom.py
@@ -0,0 +1,32 @@
+from redbot.core.bot import Red
+
+from .base_rate_responder import BaseRateResponder
+
+
+class BottomRate(BaseRateResponder):
+    title = "❯ Bottom Rate"
+    description = "{target} is {rating}% bottom"
+
+    bottom = {
+        "title": "❯ Bottom!!",
+        "description": "❯ {target} is very bottom",
+        "thumbnail": r"https://cdn.discordapp.com/emojis/1093690664119717939.webp?size=512&quality=lossless",
+    }
+
+    user_overrides = {
+        # Butts
+        641228870795657256: bottom,
+        # Jessi
+        117012698964819976: bottom,
+        # Ryan
+        843188175596945429: bottom,
+        # Kirin
+        140186220255903746: bottom,
+        # ice
+        819276102325239840: bottom,
+    }
+
+    def __init__(self, parent, bot: Red):
+        self.parent = parent
+        self.bot = bot
+        super().__init__(parent, bot)

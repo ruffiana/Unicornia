7c2fafeb3558fd3140a16a3eb48f8e834e242fe2
Ruffiana <ruffiana.plays@gmail.com>
2025-01-16 20:40:26 -0600

    Refactor ResponderCog to enhance member targeting and streamline responder checks

--- responder/main.py
+++ responder/main.py
@@ -1,22 +1,27 @@
+import glob
+import importlib
 import logging
+from pathlib import Path
 import re
 
 import discord
-
 from redbot.core import commands
 from redbot.core.bot import Red
 
 from . import __version__
-import importlib
-import glob
-from pathlib import Path
-from .text_responders import TextResponder
 from . import const
+from .responders.text_responder_base import TextResponderBase
+from .responders.rate_base import RateBase
 
 
 class ResponderCog(commands.Cog):
     RESPONDERS_PATH = Path(__file__).parent / "responders"
     RESPONDER_FILE_PATHS = [Path(p) for p in glob.glob(str(RESPONDERS_PATH / "*.py"))]
+
+    # Pattern used to separate potential commands from target members
+    # ^(.*?): Captures any characters (non-greedy) at the beginning of the string as trigger.
+    # (<@!?\d+>|@\w+|@\w+\s\w+)$: Matches and captures the member part, which can be a user mention, user ID, or username.
+    COMMAND_USER_PATTERN = re.compile(r"^(.*?)(<@!?\d+>|@\w+|@\w+\s\w+)$")
 
     def __init__(self, bot: Red):
         self.bot = bot
@@ -48,8 +53,8 @@ class ResponderCog(commands.Cog):
                 attr = getattr(module, attr_name)
                 if (
                     isinstance(attr, type)
-                    and attr is not TextResponder
-                    and issubclass(attr, TextResponder)
+                    and attr is not any([TextResponderBase, RateBase])
+                    and issubclass(attr, TextResponderBase)
                     and getattr(attr, "enabled", False)
                 ):
                     self.logger.debug(f'Adding "{attr_name}" to responders')
@@ -58,17 +63,53 @@ class ResponderCog(commands.Cog):
 
         return responders
 
+    def get_target_member(self, message, arg):
+        member = None
+
+        # Try to get member by mention
+        if re.match(r"<@!?\d+>", arg):
+            member_id = int(re.findall(r"\d+", arg)[0])
+            member = message.guild.get_member(member_id)
+
+        # Try to get member by user ID
+        if not member and arg.isdigit():
+            member = message.guild.get_member(int(arg))
+
+        # Try to get member by username
+        if not member:
+            for m in message.guild.members:
+                if str(m) == arg or f"{m.name}#{m.discriminator}" == arg:
+                    member = m
+                    break
+
+        return member
+
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message) -> None:
         # Ignore messages from all bots
         if message.author.bot:
             return
 
+        match = self.COMMAND_USER_PATTERN.match(message.content.strip())
+        if match:
+            trigger = match.group(1).strip()
+            target = match.group(2).strip()
+        else:
+            trigger = message.content
+            target = None
+
+        if target:
+            target_member = self.get_target_member(message, target)
+            if target_member is None:
+                return await message.reply(f'Unable to find a member using "target".')
+        else:
+            target_member = message.author
+
         for responder in self.responders:
-            self.logger.debug(f"Checking responder: {responder}")
-            flags = re.IGNORECASE if responder.ignore_case else 0
-            self.logger.debug(f"Checking pattern: {responder.pattern}")
-            match = re.search(responder.pattern, message.content, flags)
+            self.logger.debug(
+                f"Checking responder: {responder} using pattern: {responder.pattern}"
+            )
+            match = re.search(responder.pattern, trigger, responder.regex_flags)
             if match:
                 self.logger.debug(f"Match: {match}")
-                return await responder.response(message)
+                return await responder.respond(message, target=target_member)

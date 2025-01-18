e31897eb92b2fddffc7494a334b586f7eeb79f2f
Ruffiana <ruffiana.plays@gmail.com>
2025-01-18 11:48:01 -0600

    Refactor ResponderCog to improve member retrieval logic and enhance pattern matching validation

--- responder/main.py
+++ responder/main.py
@@ -1,18 +1,17 @@
 import glob
 import importlib
 import logging
-from pathlib import Path
 import re
+from pathlib import Path
 from typing import Union
 
 import discord
 from redbot.core import commands
 from redbot.core.bot import Red
 
-from . import __version__
-from . import const
-from .responders.base_text_responder import BaseTextResponder
+from . import __version__, const
 from .responders.base_rate_responder import BaseRateResponder
+from .responders.base_text_responder import BaseTextResponder
 
 
 class ResponderCog(commands.Cog):
@@ -38,9 +37,11 @@ class ResponderCog(commands.Cog):
     # \s+: Matches one or more whitespace characters.
     # (<@!?\d{17,19}>|\d{17,19}|@\w+|@\w+\s\w+): Matches and captures the member part,
     # which can be a user mention, user ID, or username.
-    COMMAND_USER_PATTERN = re.compile(
-        r"^(.*?)\s+(<@!?\d{17,19}>|\d{17,19}|@\w+|@\w+\s\w+)$"
-    )
+    # COMMAND_USER_PATTERN = re.compile(
+    #     r"^(.*?)\s+(<@!?\d{17,19}>|\d{17,19}|@\w+|@\w+\s\w+)$"
+    # )
+    # using just the user mention/ user ID pattern for now
+    COMMAND_USER_PATTERN = re.compile(r"^(.*?)\s+(<@!?\d{17,19}>|\d{17,19})$")
 
     def __init__(self, bot: Red):
         self.bot = bot
@@ -82,27 +83,27 @@ class ResponderCog(commands.Cog):
 
         return responders
 
-    async def get_target_member(self, message, arg):
+    async def get_target_member(self, message, target_key):
         member = None
 
         # Try to get member by mention
-        if re.match(r"<@!?\d+>", arg):
-            member_id = int(re.findall(r"\d+", arg)[0])
+        if re.match(r"<@!?\d+>", target_key):
+            member_id = int(re.findall(r"\d+", target_key)[0])
             member = message.guild.get_member(member_id)
 
         # Try to get member by user ID
-        if not member and arg.isdigit():
-            member = message.guild.get_member(int(arg))
+        if not member and target_key.isdigit():
+            member = message.guild.get_member(int(target_key))
             if not member:
                 try:
-                    member = await message.guild.fetch_member(int(arg))
+                    member = await message.guild.fetch_member(int(target_key))
                 except discord.NotFound:
                     member = None
 
         # Try to get member by username
         if not member:
             for m in message.guild.members:
-                if str(m) == arg or f"{m.name}#{m.discriminator}" == arg:
+                if str(m) == target_key or f"{m.name}#{m.discriminator}" == target_key:
                     member = m
                     break
 
@@ -126,27 +127,34 @@ class ResponderCog(commands.Cog):
             trigger = message.content
             target = None
 
-        if target:
-            target_member = await self.get_target_member(message, target)
-            if target_member is None:
-                return await message.reply(f'Unable to find a member using "{target}".')
-        else:
-            target_member = message.author
-
         for responder in self.responders:
             self.logger.debug(
-                f"Checking responder: {responder} using pattern: {responder.pattern}"
+                f"Checking responder: {responder} using patterns: {responder.patterns}"
             )
 
-            # Check if the responder has a list of patterns
-            if hasattr(responder, "patterns") and responder.patterns:
-                for pattern in responder.patterns:
-                    match = re.search(pattern, trigger, responder.regex_flags)
-                    if match:
-                        self.logger.debug(f"Match: {match}")
-                        return await responder.respond(message, target=target_member)
-            else:
-                match = re.search(responder.pattern, trigger, responder.regex_flags)
-                if match:
-                    self.logger.debug(f"Match: {match}")
-                    return await responder.respond(message, target=target_member)
+            # Make sure the responder has a valid list patterns to match against
+            if not hasattr(responder, "patterns") or not responder.patterns:
+                self.logger.error(f"Responder {responder} has no patterns!")
+                continue
+
+            for pattern in responder.patterns:
+                match = re.search(pattern, trigger, responder.regex_flags)
+                if not match:
+                    self.logger.debug(f"No match for {trigger} in pattern: {pattern}")
+                    continue
+
+                # We have a valid responder pattern and a target key
+                if target:
+                    target_member = await self.get_target_member(message, target)
+                    if target_member is None:
+                        return await message.reply(
+                            f'Unable to find a member using "{target}".'
+                        )
+                # No target key, use the message author
+                else:
+                    target_member = message.author
+
+                self.logger.debug(
+                    f"message: {message}, target_member: {target_member}, match: {match}"
+                )
+                return await responder.respond(message, target_member, match)

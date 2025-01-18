cc16e6365906298504712cf3629d42e8a557bebd
Ruffiana <ruffiana.plays@gmail.com>
2025-01-18 12:50:56 -0600

    Add user ID lists for always and never respond in const.py

--- responder/const.py
+++ responder/const.py
@@ -5,3 +5,19 @@ LOG_LEVEL = logging.INFO
 UNICORNIA_BOT_COLOR = 5778572
 PRIDE_EMOJI = "🏳️‍🌈"
 PRIDE_HEART = "https://cdn.discordapp.com/emojis/1088555199146242248.webp?size=128&quality=lossless"
+
+# these are the user IDs of the people who are always responded to
+# they can be extended in the individual responder classes
+ALWAYS_RESPOND = [
+    # ruffiana
+    474075064069783552
+]
+
+# these are the user IDs of the people who are never responded to
+# they can be extended in the individual responder classes
+NEVER_RESPOND = [
+    # junny
+    89582933735665664,
+    # sienna
+    352087314282971136,
+]

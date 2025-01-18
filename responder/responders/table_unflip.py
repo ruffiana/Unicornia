c5f97d38b17ea41adff9f27bcfb329341357b6b6
Ruffiana <ruffiana.plays@gmail.com>
2025-01-18 11:47:04 -0600

    Refactor TableUnflipResponder to support multiple patterns and update respond method signature

--- responder/responders/table_unflip.py
+++ responder/responders/table_unflip.py
@@ -1,3 +1,5 @@
+import re
+
 import discord
 from redbot.core.bot import Red
 
@@ -6,7 +8,7 @@ from .base_text_responder import BaseTextResponder
 
 class TableUnflipResponder(BaseTextResponder):
     enabled = True
-    pattern = r"\(╯°□°\)╯︵ ┻━┻"
+    patterns = [r"\(╯°□°\)╯︵ ┻━┻"]
     ignore_case = True
 
     # List of user IDs that will never get a response
@@ -17,7 +19,12 @@ class TableUnflipResponder(BaseTextResponder):
         self.parent = parent
         self.bot = bot
 
-    async def respond(self, message: discord.Message, target: discord.Member = None):
+    async def respond(
+        self,
+        message: discord.Message,
+        target: discord.Member,
+        match: re.Match,
+    ):
         if message.author.id in self.never_respond:
             return
 

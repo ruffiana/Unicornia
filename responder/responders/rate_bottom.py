a90e548114b7f06e88e8c4c98c8e5ec2a6ae7d4d
Ruffiana <ruffiana.plays@gmail.com>
2025-01-18 14:58:57 -0600

    Refactor user_overrides in BottomRate to replace duplicate Kirin entry with correct ID

--- responder/responders/rate_bottom.py
+++ responder/responders/rate_bottom.py
@@ -1,6 +1,7 @@
 from redbot.core.bot import Red
 
 from .base_rate_responder import BaseRateResponder
+from .. import const
 
 
 class BottomRate(BaseRateResponder):
@@ -14,14 +15,14 @@ class BottomRate(BaseRateResponder):
     }
 
     user_overrides = {
+        # Kirin
+        const.KIRIN_ID: bottom,
         # Butts
         641228870795657256: bottom,
         # Jessi
         117012698964819976: bottom,
         # Ryan
         843188175596945429: bottom,
-        # Kirin
-        140186220255903746: bottom,
         # ice
         819276102325239840: bottom,
     }

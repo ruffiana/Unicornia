ae6a47cd7afdd2272073ee97eab4c18d625bfa15
ruffiana <51935769+ruffiana@users.noreply.github.com>
2025-01-18 18:07:56 -0600

    Update long_cat.py
    Changed length from random int to number of 'o's

--- responder/responders/long_cat.py
+++ responder/responders/long_cat.py
@@ -15,7 +15,7 @@ from .. import const
 
 class LongCatResponder(BaseTextResponder):
     enabled = True
-    patterns = [r"\blong cat\b", r"\blong kitty\b"]
+    patterns = [r"\bl(o+)ng cat\b", r"\bl(o+)ng kitty\b"]
     ignore_case = True
 
     EMOJI_CAT_FRONT = "<:longcat_1:948672953715929168>"
@@ -36,12 +36,16 @@ class LongCatResponder(BaseTextResponder):
         target: discord.Member,
         match: re.Match,
     ):
-        sections = random.randint(0, 3)
+        
+        #sections = random.randint(0, 3)
+        ooo = match.group(1)
+        sections = len(ooo)
         middle_cat = "".join([self.EMOJI_CAT_MIDDLE] * sections)
         long_cat = f"{self.EMOJI_CAT_FRONT}{middle_cat}{self.EMOJI_CAT_END}"
 
         # critical fail!
-        if random.randrange(0, 20) == 1:
+        # if random.randrange(0, 20) == 1:
+        if sections > 4:
             middle_cat = "".join([self.EMOJI_CAT_MIDDLE] * 2)
             middle_cat = (
                 f"{self.EMOJI_CAT_MIDDLE}{self.EMOJI_KNIFE}{self.EMOJI_CAT_MIDDLE}"

1eea6523e04112160846c19d9a084d9b724a6fd8
Ruffiana <ruffiana.plays@gmail.com>
2025-01-21 18:22:35 -0600

    Refactor responder handling and add methods for retrieving target members

--- responder/main.py
+++ responder/main.py
@@ -33,15 +33,10 @@ class ResponderCog(commands.Cog):
         686092688059400454,
     }
 
-    # Pattern used to separate potential commands from target members
+    # Pattern used to separate potential trigger from the target member
     # ^(.*?): Captures any characters (non-greedy) at the beginning of the string as trigger.
     # \s+: Matches one or more whitespace characters.
-    # (<@!?\d{17,19}>|\d{17,19}|@\w+|@\w+\s\w+): Matches and captures the member part,
-    # which can be a user mention, user ID, or username.
-    # COMMAND_USER_PATTERN = re.compile(
-    #     r"^(.*?)\s+(<@!?\d{17,19}>|\d{17,19}|@\w+|@\w+\s\w+)$"
-    # )
-    # using just the user mention/ user ID pattern for now
+    # (<@!?\d{17,19}>|\d{17,19}): Matches and captures a mention or user ID.
     COMMAND_USER_PATTERN = re.compile(r"^(.*?)\s+(<@!?\d{17,19}>|\d{17,19})$")
 
     def __init__(self, bot: Red):
@@ -86,24 +81,21 @@ class ResponderCog(commands.Cog):
 
         return responders
 
-    @commands.Cog.listener()
-    async def on_message(self, message: discord.Message) -> None:
-        # Ignore messages from all bots
-        if message.author.bot:
-            return
+    def _get_responder(
+        self, trigger: str
+    ) -> Union[tuple[BaseTextResponder, re.Match], None]:
+        """Retrieve the appropriate responder based on the given trigger.
 
-        # Check if the message is in an allowed channel
-        if message.channel.id not in self.ALLOWED_CHANNEL_IDS:
-            return
+        This method iterates through the list of responders and checks if any of them
+        have patterns that match the provided trigger string. If a match is found, the
+        corresponding responder is returned. If no match is found, None is returned.
 
-        match = self.COMMAND_USER_PATTERN.match(message.content.strip())
-        if match:
-            trigger = match.group(1).strip()
-            target_key = match.group(2).strip()
-        else:
-            trigger = message.content
-            target_key = None
+        Args:
+            trigger (str): The trigger string to match against the responder patterns.
 
+        Returns:
+            Union[BaseTextResponder, None]: The responder that matches the trigger, or None if no match is found.
+        """
         for responder in self.responders:
             self.logger.debug(
                 f"Checking responder: {responder} using patterns: {responder.patterns}"
@@ -116,36 +108,97 @@ class ResponderCog(commands.Cog):
 
             for pattern in responder.patterns:
                 match = re.search(pattern, trigger, responder.regex_flags)
-                if not match:
-                    self.logger.debug(f"No match for {trigger} in pattern: {pattern}")
-                    continue
-
-                # Check if the responder is on cooldown
-                if responder.is_on_cooldown():
-                    if responder.silent_cooldown:
-                        return
-                    return await message.reply(
-                        f"Please wait {responder.get_cooldown_remaining()}s before using this command again."
-                    )
-
-                # We have a valid responder pattern and a target key
-                if target_key:
-                    context = await self.bot.get_context(message)
-                    target_member = await unicornia_discord.get_member(
-                        context, target_key
-                    )
-                    if target_member is None:
-                        return await message.reply(
-                            f'Unable to find a member using "{target_key}".'
-                        )
-                # No target key, use the message author
+                if match:
+                    return responder, match
                 else:
-                    target_member = message.author
+                    self.logger.debug(f"No match for {trigger} in pattern: {pattern}")
 
-                self.logger.debug(
-                    f"message: {message}, target_member: {target_member}, match: {match}"
-                )
+        return None
 
-                # Update the last called time for the responder and call the respond method
-                responder.update_last_called()
-                return await responder.respond(message, target_member, match)
+    async def _get_target_member(
+        self, message: discord.Message, target_key: Union[str, None]
+    ) -> discord.Member:
+        """Retrieves the target member from a message.
+
+        If the target_key is None, the author of the message is returned.
+        Otherwise, it attempts to find and return the member specified by the target_key.
+
+        Args:
+            message (discord.Message): The message object containing the author and context.
+            target_key (Union[str, None]): The key used to identify the target member. If None, the message author is returned.
+
+        Returns:
+            discord.Member: The member object corresponding to the target_key or the message author if target_key is None.
+        """
+        if target_key is None:
+            return message.author
+
+        context = await self.bot.get_context(message)
+        return await unicornia_discord.get_member(context, target_key)
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message) -> None:
+        """Event listener that is called when a message is created and sent to a channel.
+
+        Args:
+            message (discord.Message): The message that was sent.
+
+        Returns:
+            None
+
+        Behavior:
+            - Ignores messages sent by bots.
+            - Ignores messages sent in channels not listed in ALLOWED_CHANNEL_IDS.
+            - Extracts the trigger and target key from the message content using COMMAND_USER_PATTERN.
+            - Finds a responder for the trigger.
+            - Checks if the responder is on cooldown and replies with a cooldown message if necessary.
+            - Attempts to find the target member using the target key.
+            - Updates the responder's last called time and calls the responder's respond method.
+        """
+
+        # Ignore messages from all bots
+        if message.author.bot:
+            return
+
+        # Check if the message is in an allowed channel
+        if message.channel.id not in self.ALLOWED_CHANNEL_IDS:
+            return
+
+        # Separate trigger from potential target member
+        match = self.COMMAND_USER_PATTERN.match(message.content.strip())
+        if match:
+            trigger = match.group(1).strip()
+            target_key = match.group(2).strip()
+        else:
+            trigger = message.content
+            target_key = None
+
+        # Try and find a responder for any triggers in the message
+        responder, responder_match = self._get_responder(trigger)
+        if responder is None:
+            return
+
+        # Check if the responder is on cooldown
+        if (
+            responder.is_on_cooldown()
+            and not message.author.guild_permissions.administrator
+        ):
+            if responder.silent_cooldown:
+                return
+            return await message.reply(
+                f"Please wait {responder.get_cooldown_remaining()}s before using this command again."
+            )
+
+        # Get the target discord.Member object
+        target_member = await self._get_target_member(message, target_key)
+        if target_member is None:
+            await message.reply(f'Unable to find a member using "{target_key}".')
+            return None
+
+        # Update the last called time for the responder and call the respond method
+        responder.update_last_called()
+
+        self.logger.debug(
+            f"Calling responder: {responder}\nmessage: {message.content}\ntarget_member: {target_member}\nmatch: {responder_match}"
+        )
+        return await responder.respond(message, target_member, responder_match)

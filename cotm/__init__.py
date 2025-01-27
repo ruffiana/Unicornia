4d14a2dddd8ce8257486b77324c5aba65e9ed5c6
Ruffiana <ruffiana.plays@gmail.com>
2025-01-27 13:38:03 -0600

    Add Cutie of the Month Contest Cog with initial setup

---
+++ cotm/__init__.py
@@ -0,0 +1,12 @@
+"""Cutie of the Month Contest Cog"""
+
+__version__ = "1.0.0"
+__author__ = "Unicornia Team"
+__credits__ = ["Ruffiana"]
+__license__ = "MIT"
+
+from .main import ContestCog
+
+
+async def setup(bot):
+    await bot.add_cog(ContestCog(bot))
